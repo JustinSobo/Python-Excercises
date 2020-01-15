3023b1fb87d3c1e18301677c0d81fe494c748a58
Justin Stevens <37387224+JustinSobo@users.noreply.github.com>
2020-01-14 19:57:30 -0800

    Create d. Comments Exercise 2.py

---
+++ d. Comments Exercise 2.py
@@ -0,0 +1,13 @@
+# Exercise:
+# Use a multiline string to make the a multi line comment:
+# This is a comment
+# written in 
+# more that just one line
+
+# Solution:
+
+'''
+This is a comment
+written in 
+more that just one line
+'''
