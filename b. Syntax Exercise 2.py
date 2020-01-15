c200c1d10166d7e82c601055da8a44a0e73e6d0e
Justin Stevens <37387224+JustinSobo@users.noreply.github.com>
2020-01-14 19:51:18 -0800

    Create b. Syntax Exercise 2.py

---
+++ b. Syntax Exercise 2.py
@@ -0,0 +1,12 @@
+# Exercise:
+# This example misses indentations to be correct.
+
+# Insert the missing indentation to make the code correct:
+
+# if 5 > 2:
+# print("Five is greater than two!")
+
+Solution:
+
+if 5 > 2:
+  print("Five is greater than two!")
