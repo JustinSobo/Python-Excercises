5789f6642c0a2c3ddef86522883fb1a45138a2f1
Justin Stevens <37387224+JustinSobo@users.noreply.github.com>
2020-01-16 17:05:00 -0500

    Update s. Numbers Exercise 3.py

--- s. Numbers Exercise 3.py
+++ s. Numbers Exercise 3.py
@@ -1,1 +1,14 @@
-# LOST CODE... GO BACK AND DO EXERCISE AGAIN...
+# Exercise:
+# Insert the correct syntax to convert x into a complex number.
+
+# x = 5
+# x = ___(x)
+
+# Solution:
+x = 5
+x = complex(x)
+
+# Extra Notes:
+# x = 1    # int
+# y = 2.8  # float
+# z = 1j   # complex
