def0493f25729eb6b6fd424a30ed55d0adb60ec3
Justin Stevens <37387224+JustinSobo@users.noreply.github.com>
2020-01-16 17:13:45 -0500

    Create q. Strings Exercise 2.py

---
+++ q. Strings Exercise 2.py
@@ -0,0 +1,10 @@
+Exercise:
+Get the first character of the string txt.
+txt = "Hello World"
+x = ___
+
+Solution:
+
+x = txt[0]
+
+# Rememebr most programming languages including Python use 0 to describe the "first" of something in a list. (Similar to binary...)
