1c17329c71665a6f74ba98d7f8a59c6be5abdd42
Justin Stevens <37387224+JustinSobo@users.noreply.github.com>
2020-01-16 17:23:07 -0500

    Update x. Strings Exercise 3.py

--- x. Strings Exercise 3.py
+++ x. Strings Exercise 3.py
@@ -1,1 +1,10 @@
+# Exercise:
+# Return the string without any whitespace at the beginning or the end.
+# txt = " Hello World "
+# x = ___
 
+# Solution:
+txt = " Hello World "
+x = txt.strip()
+  
+# The strip function removes whitesapce.. I didn't know that.. I do now..
