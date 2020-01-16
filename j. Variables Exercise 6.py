d64cc668ec9da47e0d4b995ae2378bda7248217d
Justin Stevens <37387224+JustinSobo@users.noreply.github.com>
2020-01-16 10:10:38 -0800

    Create j. Variables Exercise 6.py

---
+++ j. Variables Exercise 6.py
@@ -0,0 +1,7 @@
+# Exercise:
+# Insert the correct syntax to assign the same value to all three variable in one code line.
+# x_ y_ z_ "Orange"
+
+x = y = z = "Orange"
+
+# My take away is that I can set multiple variables equal to one another and have them equal to "something else". Function, string, etc...
