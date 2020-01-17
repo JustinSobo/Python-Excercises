f130383698df5ba41eeb83701818cb4718b380da
Justin Stevens <37387224+JustinSobo@users.noreply.github.com>
2020-01-17 17:51:49 -0500

    Create zb. Strings Exercise 8.py

---
+++ zb. Strings Exercise 8.py
@@ -0,0 +1,10 @@
+#Exercise:
+#Insert the correct syntax to add a placeholder for the age parameter.
+#age = 36
+#txt = "My name is John, and I am "
+#print(txt.format(age))
+
+Solution:
+age = 36
+txt = "My name is John, and I am {}"
+print(txt.format(age))
