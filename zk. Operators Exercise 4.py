7112613c68fdcb938c6c315ca2efc350a9fff83f
Justin Stevens <37387224+JustinSobo@users.noreply.github.com>
2020-01-17 18:03:59 -0500

    Create zk. Operators Exercise 4.py

---
+++ zk. Operators Exercise 4.py
@@ -0,0 +1,8 @@
+#Exercise:
+#Use the correct comparison operator to check if 5 is not equal to 10.
+#if 5___10:
+  print("5 and 10 is not equal")
+  
+#Solution:
+if 5 != 10:
+  print("5 and 10 is not equal")
